b217169bcec0f1ca394eaa4420a793398446de70
Nathan Ateka <nathanateka81@gmail.com>
2025-07-10 22:19:32 +0300

    added products

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,7 +3,8 @@ import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { motion } from "framer-motion";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { VITE_API_BASE_URL } from "../constants/URLs";
+// import { VITE_API_BASE_URL } from "../constants/URLs";
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001';
 
 export default function LoginPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
