727f5c72c55ecd6c751891f0afc0c371b2e865b5
Nathan Ateka <nathanateka81@gmail.com>
2025-07-19 16:04:01 +0300

    removed the src folder added by the other branch in the root folder

--- src/contexts/AuthContext.tsx
+++
@@ -1,2 +0,0 @@
-import { AuthContext } from './AuthContextDef';
-// Only export the AuthProvider component from this file 
--- src/contexts/CartContext.tsx
+++
@@ -1,2 +0,0 @@
-import { CartContext } from './CartContextDef';
-// Only export the CartProvider component from this file 
